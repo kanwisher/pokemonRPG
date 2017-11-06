a94038fc72a23dc4a2b056042b4e390bf9d15c60
kanwisher <kanwisher@gmail.com>
2017-11-06 00:00:31 -0500

    fail

--- pokemonRPG/assets/scripts/script-main.js
+++ pokemonRPG/assets/scripts/script-main.js
@@ -1,2 +1,30 @@
-alert("'welcome to the pokemon rpg game' -professor oak");
-alert("to disable the alerts just click ok");
+// prompt(`
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 'welcome to the pokemon rpg game!"
+// --professor oak
+
+
+
+
+
+
+// ~~~~~~~~~~~~~~
+
+
+
+
+
+// (just hit cancel, messed up this part)
+
+// `);
+// alert(`
+
+
+
+
+// to disable the alerts just click ok`);
